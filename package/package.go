cb9c2af09cd0403b90534c0587f8359419436b08
jurgen.kluft <jurgen.kluft@gmail.com>
2024-06-06 10:29:03 +0800

    * unittest is compiling/linking

--- package/package.go
+++ package/package.go
@@ -23,6 +23,7 @@ func GetPackage() *denv.Package {
 
 	// 'cmath' unittest project
 	maintest := denv.SetupDefaultCppTestProject("cmath_test", "github.com\\jurgen-kluft\\cmath")
+	maintest.AddDefine("CGLM_DEFINE_PRINTS")
 	maintest.Dependencies = append(maintest.Dependencies, cunittestpkg.GetMainLib())
 	maintest.Dependencies = append(maintest.Dependencies, cbasepkg.GetMainLib())
 	maintest.Dependencies = append(maintest.Dependencies, mainlib)
